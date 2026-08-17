48fe9d0e1a18b72fbfc3e1fcefabd001403a5de5
Scar044 <allisson.alvarado2027@frech.superate.org>
2026-08-17 10:44:30 -0600

    practica js

---
+++ Practica1JS/JS/script.js
@@ -0,0 +1,21 @@
+document.write("Hola mundo");
+
+alert("Mundo Hola");
+
+console.log("Hola chavales");
+
+let nombre = "Ana";
+
+let edad = 18;
+
+let estudiante = true;
+
+let ciudad;
+
+let mascota = null;
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof estudiante);
+console.log(typeof ciudad);
+console.log(typeof mascota);
